4e8248e861e526e5a65c60bb0bd41d81e99960d6
Ivan Radoš <irados@autozubak.hr>
2024-07-04 20:00:53 +0200

    Add project files.

---
+++ OL-OASP_dev_H-07_23_WebApi/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace OL_OASP_dev_H_07_23_WebApi
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
